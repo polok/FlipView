43d4304889d744033c99f7929b8a6434027fa2bc
marcin.polak <mpolak87@gmail.com>
2014-10-29 21:05:05 +0100

    Added listener example

--- demo/src/main/java/pl/polak/flipview/demo/MainActivity.java
+++ demo/src/main/java/pl/polak/flipview/demo/MainActivity.java
@@ -2,6 +2,9 @@ package pl.polak.flipview.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
+
+import com.github.polok.flipview.FlipView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        ((FlipView)findViewById(R.id.flip_listener_view_demo)).setFlipViewChangeListener(new FlipView.FlipViewChangeListener() {
+            @Override
+            public void onFlipViewClick(FlipView flipView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "Clicked - " + isChecked, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
